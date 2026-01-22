d4dd5d9c1b6491046d278c28368c0ea60d0591fe
Yash Raj Gupta <yashyrg01@gmail.com>
2026-01-22 16:05:10 +0530

    adding stag and prod deploy file

---
+++ deploy-prod.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+APP_DIR="/srv/apps/wordpress/prod"
+BRANCH="live"
+
+echo "===== Deploying prod ====="
+cd $APP_DIR
+
+git fetch origin
+git checkout $BRANCH
+git pull origin $BRANCH
+
+#checking deploy
+echo "→ Restarting containers"
+docker-compose down
+docker-compose up -d
+
+echo "✅ PROD deployment complete"
---
+++ deploy-stag.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+APP_DIR="/srv/apps/wordpress/stag"
+BRANCH="stag"
+
+echo "===== Deploying stag ====="
+cd $APP_DIR
+
+git fetch origin
+git checkout $BRANCH
+git pull origin $BRANCH
+
+#checking deploy
+echo "→ Restarting containers"
+docker-compose down
+docker-compose up -d
+
+echo "✅ STAG deployment complete"
