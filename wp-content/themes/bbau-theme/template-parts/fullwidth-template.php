3048444fa746d857a0e70c1b9cbcbcbea64e8fce
Yash Raj Gupta <yashyrg01@gmail.com>
2026-02-03 17:07:38 +0530

    full width template

--- wp-content/themes/bbau-theme/template-parts/fullwidth-template.php
+++ wp-content/themes/bbau-theme/template-parts/fullwidth-template.php
@@ -11,11 +11,11 @@
     <!-- Banner -->
     <?php get_template_part( 'banners/about-banner' ); ?>
     <!-- End Banner -->
-    <div class="container-fluid py-lg-5 page-bg page-template-about-bg min_xl_h_1172 min_sm_h_1469 overflow-hidden">
+    <div class="container-fluid py-lg-5 page-bg page-template-about-bg overflow-hidden">
         <div class="container px-lg-3 px-0 py-4 max_xl_w_1280 position-relative">
             <div class="row py-lg-2 py-3">
                 <div class="col-lg-12">
-                    <h3><?php the_title()?></h3>
+                    <h3 class="mb-3"><?php the_title()?></h3>
                     <?php 
                   if ( have_posts() ) : 
                      while ( have_posts() ) : the_post();
