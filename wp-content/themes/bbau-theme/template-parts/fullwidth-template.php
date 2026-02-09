4e24233c33ed18620eaf0aa9a608ee60f0cb68de
Yash Raj Gupta <yashyrg01@gmail.com>
2026-02-09 19:29:39 +0530

    visitor

--- wp-content/themes/bbau-theme/template-parts/fullwidth-template.php
+++ wp-content/themes/bbau-theme/template-parts/fullwidth-template.php
@@ -13,7 +13,7 @@
     <!-- End Banner -->
     <div class="container-fluid py-lg-5 page-bg page-template-about-bg overflow-hidden">
         <?php get_template_part('template-parts/breadcrumb'); ?>
-        <div class="container px-lg-3 px-0 py-4 max_xl_w_1280 position-relative">
+        <div class="container px-lg-3 px-0 max_xl_w_1280 position-relative">
             <div class="row py-lg-2 py-3">
                 <div class="col-lg-12">
                     <h3 class="mb-3"><?php the_title()?></h3>
