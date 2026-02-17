f626e60469c7dd3a1833f7bc40a5df9736487834
Yash Raj Gupta <yashyrg01@gmail.com>
2026-02-17 15:41:13 +0530

    vc template

--- wp-content/themes/bbau-theme/template-parts/vc-template.php
+++ wp-content/themes/bbau-theme/template-parts/vc-template.php
@@ -6,6 +6,7 @@ Template Name: VC Template
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 get_header();
+$page_Id = get_the_ID();
 ?>
 <?php get_template_part('banners/about-banner'); ?>
 <div class="container-fluid page-bg page-template-about-bg py-lg-5 overflow-hidden">
@@ -18,8 +19,7 @@ get_header();
                 <!-- Left Image -->
                 <div class="vc-hero-image">
                     <div class="vc-image-wrapper">
-                        <img src="/wp-content/uploads/2026/02/Image-Vice-Chancellor.png"
-                            alt="Vice Chancellor">
+                        <img src="/wp-content/uploads/2026/02/Image-Vice-Chancellor.png" alt="Vice Chancellor">
                     </div>
                 </div>
 
@@ -49,185 +49,151 @@ get_header();
 
     <section class="vc-message">
         <div class="vc-container1">
-            <h2>Message from the Vice Chancellor</h2>
+            <h2><?php echo get_field('vc_message_heading'); ?></h2>
             <div class="vc-message-box">
-                <p>
-                    Welcome to Babasaheb Bhimrao Ambedkar University, a premier institution of higher learning dedicated
-                    to
-                    shaping the minds of future leaders. As we strive for excellence in education, research and
-                    innovations,
-                    we remain focused on fostering a culture of innovation, inclusivity, and social responsibility
-                    essential
-                    to navigate the complexities of the 21st century and provide the opportunity to everyone to realize
-                    one’s full potential. Dr. Ambedkar’s philosophy of social justice and equality serves as a beacon,
-                    guiding us in carrying out our teaching, research and extension activities to build an inclusive
-                    society
-                    where diverse voices are heard and valued. Our focus is to develop the capabilities of our students
-                    to
-                    enable them to understand and analyse the ever-changing environment in which they shall be living
-                    and
-                    working for effective decision making while maintaining the higher domains of human values.This
-                    shall
-                    enable us to realize the potential benefits of Panchkosha based education of character building and
-                    holistic personality development of students. Efforts would be to create and provide
-                    platforms/opportunities for the students through which they can hone their personality and explore
-                    the
-                    best in them. We plan to establish different hobby/activity clubs like dance, drama, music,
-                    photography,
-                    literary etc. in which students will participate and cultivate the passion of their choice thus
-                    enabling
-                    them to convert their passion into profession at a later stage. My efforts would be to work for
-                    strengthening the values such as freedom of enquiry and creativity , social justice & inclusiveness
-                    ,
-                    diversity , prudent management of resources, innovations & entrepreneurship, engaged learning and
-                    giving
-                    back to society. BBAU, is committed to unleash the transformative potential of NEP-2020 , which
-                    envisions higher education to be flexible, multidisciplinary and outcome-oriented promoting
-                    interdisciplinary learning, research & innovation and strengthening the link between Higher
-                    Education
-                    Institutions (HEIs) and industry. All our efforts would be made to ensure quality teaching,
-                    impactful
-                    research having capacity to address societal problems and outreach activities by strengthening
-                    physical
-                    infrastructure, upgrading laboratories, leveraging ICT in teaching-learning & administrative
-                    activities
-                    and creating commensurate ecosystems. Priority would be towards skill development by involving
-                    industries in the process.</p>
-                <p>
-                    Friends, as we embark on this exciting journey, I urge each one of you to embrace the spirit of
-                    innovation, entrepreneurship, and social responsibility and work together to create BBAU not only a
-                    centre of excellence but also a catalyst for positive change in our society and make this world more
-                    inclusive, fair and liveable.</p>
-                <p>
-                    I am sure with enthusiasm, active engagement, volunteering and advocacy for change by all
-                    stakeholders,
-                    we can make BBAU an institution known for its capacity to transform the society and contribute
-                    towards
-                    building a prosperous and great Bharat.
-                    Together, we can make a difference!
-                </p>
+                <?php echo get_field('vc_message');?>
             </div>
         </div>
     </section>
-    <!-- <section class="vc-video">
-    <div class="vc-container1">
-        <h2>Address by the Vice Chancellor</h2>
-        <div class="video-wrapper">
-            <iframe src="https://www.youtube.com/embed/VIDEO_ID" frameborder="0" allowfullscreen>
-            </iframe>
-        </div>
-    </div>
-</section> -->
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css" />
     <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
 
     <section class="vc-gallery">
         <div class="vc-container1">
-            <div class="swiper vcSwiper">
-
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide">
-                        <iframe width="560" height="315"
-                            src="https://www.youtube.com/embed/qYwO4PLd7jw?si=NpaGRBqWGbMRQeOF"
-                            title="YouTube video player" frameborder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                    </div>
-                    <div class="swiper-slide">
-                        <iframe width="560" height="315"
-                            src="https://www.youtube.com/embed/qYwO4PLd7jw?si=NpaGRBqWGbMRQeOF"
-                            title="YouTube video player" frameborder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                    </div>
-                    <div class="swiper-slide">
-                        <iframe width="560" height="315"
-                            src="https://www.youtube.com/embed/qYwO4PLd7jw?si=NpaGRBqWGbMRQeOF"
-                            title="YouTube video player" frameborder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                    </div>
-                    <div class="swiper-slide">
-                        <iframe width="560" height="315"
-                            src="https://www.youtube.com/embed/qYwO4PLd7jw?si=NpaGRBqWGbMRQeOF"
-                            title="YouTube video player" frameborder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                    </div>
-                    <div class="swiper-slide">hancellor/
-                        <iframe width="560" height="315"
-                            src="https://www.youtube.com/embed/qYwO4PLd7jw?si=NpaGRBqWGbMRQeOF"
-                            title="YouTube video player" frameborder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                    </div>
-                </div>
-                <div class="swiper-pagination"></div>
-
-            </div>
-        </div>
-        <div class="vc-container1">
 
             <h2>Photo Gallery</h2>
             <div class="swiper vcSwiper">
                 <div class="swiper-wrapper">
+                    <?php if( have_rows('slider_image_section') ):
+                     while ( have_rows('slider_image_section') ) : the_row(); ?>
                     <div class="swiper-slide">
-                        <img src="/wp-content/uploads/2026/02/Image-Vice-Chancellor.png" alt="">
+                        <img src="<?php echo get_sub_field('slider_images');?>" alt="">
                     </div>
-                    <div class="swiper-slide">
-                        <img src="/wp-content/uploads/2026/02/Image-Vice-Chancellor.png" alt="">
-                    </div>
-                    <div class="swiper-slide">
-                        <img src="/wp-content/uploads/2026/02/Image-Vice-Chancellor.png" alt="">
-                    </div>
-                    <div class="swiper-slide">
-                        <img src="/wp-content/uploads/2026/02/Image-Vice-Chancellor.png" alt="">
-                    </div>
-                    <div class="swiper-slide">
-                        <img src="/wp-content/uploads/2026/02/Image-Vice-Chancellor.png" alt="">
-                    </div>
+                    <?php endwhile; endif; ?>
                 </div>
                 <div class="swiper-pagination"></div>
 
             </div>
         </div>
     </section>
-    <section class="vc-lecture">
+    <!-- <section class="vc-lecture">
         <div class="vc-container1">
             <h2>Eminent Lecture Series</h2>
-
-            <div class="lecture-grid">
-                <div class="lecture-card">
-                    <h4>Bharat's Agenda for Socio-economic Development</h4>
-                    <p>Date: 02 May 2025</p>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/qYwO4PLd7jw?si=NpaGRBqWGbMRQeOF"
-                        title="YouTube video player" frameborder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <div class="swiper vcSwiper">
+                <div class="swiper-wrapper">
+                    <?php 
+                    $args = array(
+                        'post_type' => 'eminent_lecture',
+                        'posts_per_page' => -1
+                    );
+                    $lectures = new WP_Query( $args );
+                    
+                    if ( $lectures->have_posts() ) :
+                        while ( $lectures->have_posts() ) : $lectures->the_post();
+                        $video_url = trim(get_the_content());
+                    ?>
+                    <div class="swiper-slide">
+                        <div class="lecture-card">
+                            <h4><?php the_title(); ?></h4>
+                            <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>">
+                            <p>Speaker: <?php echo get_field('speaker_name'); ?></p>
+                            <p>Designation: <?php echo get_field('speaker_designation'); ?></p>
+                            <p>Date: <?php echo get_field('lecture_date'); ?></p>
+                        </div>
+                    </div>
+                    <?php 
+                        endwhile;
+                        wp_reset_postdata();
+                    endif; 
+                    ?>
                 </div>
-
-                <div class="lecture-card">
-                    <h4>A Tail of two biotechnologies: </h4>
-                    <p>Date: 21 May 2025</p>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/qYwO4PLd7jw?si=NpaGRBqWGbMRQeOF"
-                        title="YouTube video player" frameborder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                </div>
-                <div class="lecture-card">
-                    <h4>Climate Change</h4>
-                    <p>Date: 30 July 2025</p>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/qYwO4PLd7jw?si=NpaGRBqWGbMRQeOF"
-                        title="YouTube video player" frameborder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                </div>
-
+                <div class="swiper-pagination"></div>
             </div>
+    </section> -->
+
+    <section class="">
+        <div class="vc-container1">
+
+            <h2 class="vc-section-title"><?php echo get_field('heading'); ?></h2>
+
+            <div class="vc-resource-grid">
+                <?php if(have_rows('resource_section')): 
+                    while(have_rows('resource_section')): the_row();
+                    $link = get_sub_field('button_link');?>
+                    
+                <div class="vc-resource-card">
+                    <h3><?php echo get_sub_field('card_heading'); ?></h3>
+                    <p><?php echo get_sub_field('card_description'); ?></p>
+                    <a href="<?php  echo esc_url($link['url']); ?>"
+                        class="vc-btn"><?php echo esc_html($link['title']); ?></a>
+                </div>
+                <?php endwhile; endif; ?>
+            </div>
+
+        </div>
     </section>
+
 </div>
 <?php get_footer(); ?>
 
+<style>
+.vc-section-title {
+    text-align: center;
+    font-size: 28px;
+    margin-bottom: 50px;
+    color: #002855;
+}
+
+.vc-resource-grid {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 30px;
+}
+
+.vc-resource-card {
+    background: #ffffff;
+    padding: 40px 30px;
+    border-radius: 16px;
+    box-shadow: 0 8px 25px rgba(0, 0, 0, 0.07);
+    transition: all 0.3s ease;
+}
+
+.vc-resource-card:hover {
+    transform: translateY(-6px);
+    box-shadow: 0 14px 35px rgba(0, 0, 0, 0.1);
+}
+
+.vc-resource-card h3 {
+    font-size: 20px;
+    margin-bottom: 12px;
+    color: #002855;
+}
+
+.vc-resource-card p {
+    font-size: 15px;
+    color: #555;
+    margin-bottom: 20px;
+    line-height: 1.6;
+}
+
+.vc-btn {
+    text-decoration: none;
+    font-weight: 600;
+    color: #00509e;
+}
+
+.vc-btn:hover {
+    text-decoration: underline;
+}
+
+/* Responsive */
+@media (max-width: 992px) {
+    .vc-resource-grid {
+        grid-template-columns: 1fr;
+    }
+}
+</style>
 <script>
 var swiper = new Swiper(".vcSwiper", {
     spaceBetween: 20,
